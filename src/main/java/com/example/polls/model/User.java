585b053bca730933b222aa5fda83b854fb810b71
SarahNsereko <sarahnsereko59@gmail.com>
2024-03-18 09:20:17 +0300

    Running Version of part 1

--- src/main/java/com/example/polls/model/User.java
+++ src/main/java/com/example/polls/model/User.java
@@ -1,5 +1,6 @@
 package com.example.polls.model;
 
+import com.example.polls.model.audit.DateAudit;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -7,9 +8,10 @@ import jakarta.validation.constraints.Size;
 import org.hibernate.annotations.NaturalId;
 
 import java.util.HashSet;
+import java.util.Set;
 
 @Entity
-@Table(name="users",uniqueConstraints ={
+@Table(name = "users", uniqueConstraints = {
         @UniqueConstraint(columnNames = {
                 "username"
         }),
@@ -17,92 +19,92 @@ import java.util.HashSet;
                 "email"
         })
 })
-public class User extends DateAudit{
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id ;
+public class User extends DateAudit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        @NotBlank
-        @Size(max =40)
-        private String name;
+    @NotBlank
+    @Size(max = 40)
+    private String name;
 
-        @NotBlank
-        @Size (max=15)
-        private String username;
+    @NotBlank
+    @Size(max = 15)
+    private String username;
 
-        @NaturalId
-        @NotBlank
-        @Size(max=40)
-        @Email
-        private String email;
+    @NaturalId
+    @NotBlank
+    @Size(max = 40)
+    @Email
+    private String email;
 
-        @NotBlank
-        @Size(max =100)
-        private String password;
+    @NotBlank
+    @Size(max = 100)
+    private String password;
 
-        @ManyToMany(fetch=FetchType.LAZY)
-        @JoinTable(name="user_roles",
-        joinColumns=@JoinColumn(name="user_id"),
-        inverseJoinColumns =@JoinColumn(name="role_id"))
-        private Set<Role> roles =new HashSet<>();
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
-        public User() {
-        }
+    public User() {
+    }
 
-        public User(Long id, String name, String username, String email, String password, Set<Role> roles) {
-                this.id = id;
-                this.name = name;
-                this.username = username;
-                this.email = email;
-                this.password = password;
-                this.roles = roles;
-        }
+    public User(Long id, String name, String username, String email, String password, Set<Role> roles) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
 
-        public Long getId() {
-                return id;
-        }
+    public Long getId() {
+        return id;
+    }
 
-        public void setId(Long id) {
-                this.id = id;
-        }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-        public String getName() {
-                return name;
-        }
+    public String getName() {
+        return name;
+    }
 
-        public void setName(String name) {
-                this.name = name;
-        }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-        public String getUsername() {
-                return username;
-        }
+    public String getUsername() {
+        return username;
+    }
 
-        public void setUsername(String username) {
-                this.username = username;
-        }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-        public String getEmail() {
-                return email;
-        }
+    public String getEmail() {
+        return email;
+    }
 
-        public void setEmail(String email) {
-                this.email = email;
-        }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-        public String getPassword() {
-                return password;
-        }
+    public String getPassword() {
+        return password;
+    }
 
-        public void setPassword(String password) {
-                this.password = password;
-        }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-        public Set<Role> getRoles() {
-                return roles;
-        }
+    public Set<Role> getRoles() {
+        return roles;
+    }
 
-        public void setRoles(Set<Role> roles) {
-                this.roles = roles;
-        }
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
--- src/main/java/com/example/polls/repository/UserRepository.java
+++ src/main/java/com/example/polls/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package com.example.polls.repository;
 
+import com.example.polls.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
